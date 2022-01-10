4925b5cc4dd263b9f879e207592e9611c468b29a
Leger Charlie <charliel@sfr.fr>
2022-01-10 10:07:51 +0100

    [plots] Add example "plots" + CMake compliant

---
+++ app-plots/config.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_CONFIG_H
+#define PROJECT_CONFIG_H
+
+#define PROJECT_NAME "example-plots"
+#define PROJECT_VERSION "1.0.0"
+#define PROJECT_VERSION_MAJOR "1"
+#define PROJECT_VERSION_MINOR "0"
+#define PROJECT_VERSION_PATCH "0"
+
+#endif // PROJECT_CONFIG_H
